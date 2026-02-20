91388bdd93337c1a77d660113edbc3078cc3002f
JohnKingm123 <73373145+JohnKingm123@users.noreply.github.com>
2026-02-20 21:12:31 +0800

    refactor the mainFrame initialization

---
+++ 2048Demo0/src/com/jkm/game2048/App.java
@@ -0,0 +1,7 @@
+package com.jkm.game2048;
+
+public class App {
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+}
--- 2048Demo0/src/com/jkm/game2048/MainFrame.java
+++ 2048Demo0/src/com/jkm/game2048/MainFrame.java
@@ -2,31 +2,30 @@ package com.jkm.game2048;
 
 import javax.swing.*;
 
-public class GameLayout {
-    public static void main(String[] args) {
-        JFrame gameWindow = new JFrame();
-        JLabel backGround = new JLabel(
-                new ImageIcon(
-                        "E:\\Projects\\JavaSE\\Game2048\\2048Demo0\\Source\\2048Sprites\\background450_450.png"//TODO "Source Way Update"
-                )
-        );
-
-        int gameData[][]=new int[][]{
+public class MainFrame extends JFrame {
+    int gameData[][]=new int[][]{
             {0,2,2,2},
             {4,8,16,32},
             {64,64,64,64},
             {2048,0,0,0}
-        };
+    };
 
-        gameWindow.setTitle("2048");
-        gameWindow.setSize(540, 600);
-        gameWindow.setLocationRelativeTo(null);
-        gameWindow.setLayout(null);
-        gameWindow.setAlwaysOnTop(true);
-        gameWindow.setDefaultCloseOperation(3);
-        gameWindow.setVisible(true);
+    public MainFrame(){
+        initFrame();
+        paintView();
+        setVisible(true);
+    }
 
-        backGround.setBounds(40, 40, 450, 450);
+    public void initFrame(){
+        setTitle("2048");
+        setSize(540, 600);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setAlwaysOnTop(true);
+        setDefaultCloseOperation(3);
+    }
+
+    public void paintView(){
         for (int col = 0; col < 4; col++) {//TODO divide"NEW SET ADD" to 3 different Code Bars
             for (int row = 0; row < 4; row++) {
                 if(gameData[col][row]==0){
@@ -40,10 +39,17 @@ public class GameLayout {
                         )
                 );
                 numBar0.setBounds(50 + 110 * row, 50 + 110 * col, 100, 100);
-                gameWindow.getContentPane().add(numBar0);
+                super.getContentPane().add(numBar0);
 
             }
         }
-        gameWindow.getContentPane().add(backGround);
+
+        JLabel backGround = new JLabel(
+                new ImageIcon(
+                        "E:\\Projects\\JavaSE\\Game2048\\2048Demo0\\Source\\2048Sprites\\background450_450.png"//TODO "Source Way Update"
+                )
+        );
+        backGround.setBounds(40, 40, 450, 450);
+        super.getContentPane().add(backGround);
     }
 }
