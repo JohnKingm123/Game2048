07598114fa3592f4a79a922c5818672dbcc2dadb
JohnKingm123 <73373145+JohnKingm123@users.noreply.github.com>
2026-02-22 01:05:25 +0800

    MoveLogic
    hjkl move logic has been done

--- 2048Demo0/src/com/jkm/game2048/MainFrame.java
+++ 2048Demo0/src/com/jkm/game2048/MainFrame.java
@@ -1,18 +1,24 @@
 package com.jkm.game2048;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements KeyListener {
     int gameData[][] = new int[][]{
-            {0, 2, 2, 2},
-            {4, 8, 16, 32},
-            {64, 64, 64, 64},
-            {2048, 0, 0, 0}
+            {8, 0, 2, 2},
+            {4, 2, 16, 32},
+            {64, 64, 2, 64},
+            {2048, 2, 0, 2}
     };
 
     public MainFrame() {
         initFrame();
         paintView();
+
+        //bind KeyboardListener
+        this.addKeyListener(this);
+
         setVisible(true);
     }
 
@@ -26,30 +32,134 @@ public class MainFrame extends JFrame {
     }
 
     public void paintView() {
+        getContentPane().removeAll();
+
         for (int col = 0; col < 4; col++) {//TODO divide"NEW SET ADD" to 3 different Code Bars
             for (int row = 0; row < 4; row++) {
-                if (gameData[col][row] == 0) {
-                    break;
-                }
-
                 JLabel numBar0 = new JLabel(
                         new ImageIcon(
-                                "E:\\Projects\\JavaSE\\Game2048\\2048Demo0\\Source\\2048Sprites\\A_" + gameData[col][row] + "_100_100.png"
+                                "D:\\CodeStorge\\Game2048\\2048Demo0\\Source\\2048Sprites\\A_" + gameData[col][row] + "_100_100.png"
                                 //TODO "Source Way Update" and "safe file address append"
                         )
                 );
                 numBar0.setBounds(50 + 110 * row, 50 + 110 * col, 100, 100);
                 super.getContentPane().add(numBar0);
-
             }
         }
 
         JLabel backGround = new JLabel(
                 new ImageIcon(
-                        "E:\\Projects\\JavaSE\\Game2048\\2048Demo0\\Source\\2048Sprites\\background450_450.png"//TODO "Source Way Update"
+                        "D:\\CodeStorge\\Game2048\\2048Demo0\\Source\\2048Sprites\\background450_450.png"
+                        //TODO "Source Way Update"
                 )
         );
         backGround.setBounds(40, 40, 450, 450);
         super.getContentPane().add(backGround);
+
+        getContentPane().repaint();//refresh the new frame
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {//h j k l 37 38 40 39
+        int keyCode = e.getKeyCode();
+
+        if (keyCode == 37) {
+            leftMove();
+        } else if (keyCode == 38) {
+            upMove();
+        } else if (keyCode == 39) {
+            rightMove();
+        } else if (keyCode == 40) {
+            downMove();
+        }
+
+        paintView();
+    }
+
+    public void leftMove(){
+        for(int i=0;i<gameData.length;i++){
+            int[] tempStorgeArr = new int[4];
+            int index = 0;
+            for(int x = 0;x<gameData[i].length;x++){//erase "0" and all to LEFT
+                if(gameData[i][x]!=0){
+                    tempStorgeArr[index] = gameData[i][x];
+                    index++;
+                }
+            }
+
+            gameData[i] = tempStorgeArr;
+
+            for(int x = 0;x<gameData[i].length-1;x++){
+                if(gameData[i][x]==gameData[i][x+1]){
+                    gameData[i][x]*=2;
+                    for(int j = x+1;j<gameData[i].length-1;j++){
+                        gameData[i][j]=gameData[i][j+1];
+                    }
+
+                    gameData[i][gameData[i].length-1]=0;
+                }
+            }
+        }
+    }
+
+    public void rightMove(){
+        horizonalSwap();
+        leftMove();
+        horizonalSwap();
+    }
+
+    public void upMove(){
+        ctClockWise();
+        leftMove();
+        clockWise();
+    }
+
+    public void downMove(){
+        ctClockWise();
+        rightMove();
+        clockWise();
+    }
+
+    public void horizonalSwap(){
+        for(int i = 0;i<gameData.length;i++){
+            reverseArray(gameData[i]);
+        }
+    }
+
+    public void reverseArray(int[] arr){
+        for(int start = 0,end = arr.length-1;start<end;start++,end--){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end]=temp;
+        }
+    }
+
+    public void ctClockWise(){
+        int [][] newGameData = new int[4][4];
+        for(int i =0;i<newGameData.length;i++){
+            for(int j = 0;j< newGameData.length;j++){
+                newGameData[3-j][i] = gameData[i][j];
+            }
+        }
+        gameData = newGameData;
+    }
+
+    public void clockWise(){
+        int [][] newGameData = new int[4][4];
+        for(int i =0;i<newGameData.length;i++){
+            for(int j = 0;j< newGameData.length;j++){
+                newGameData[j][3-i] = gameData[i][j];
+            }
+        }
+        gameData = newGameData;
+    }
+
 }
--- first-test/src/JavaFrameTest.java
+++ first-test/src/JavaFrameTest.java
@@ -1,21 +1,113 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.Random;
 
-public class JavaFrameTest {
+
+public class JavaFrameTest extends JFrame implements  MouseListener,ActionListener, KeyListener {
+    private JButton btn0 = new JButton();
+
+    public JavaFrameTest(){
+        initFrame();
+        setVisible(true);
+    }
+
+    private void initFrame(){
+        setSize(540, 600);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setAlwaysOnTop(true);
+        setDefaultCloseOperation(3);
+        setTitle("2048");//窗体标题 左上
+        setLocationRelativeTo(null);//窗体居中
+        setAlwaysOnTop(true);//窗体永远在顶层
+        btn0.setBounds(0,0,50,50);
+
+        super.getContentPane().add(btn0);
+        btn0.addActionListener(this);
+        btn0.addMouseListener(this);
+
+        this.setFocusable(true);//Focus on Frame , not "btn0"
+        this.addKeyListener(this);
+    }
+
     public static void main(String[] args) {
-        JFrame firstFrame = new JFrame();
-        firstFrame.setSize(640,540);
-        firstFrame.setVisible(true);//窗体可见
-        firstFrame.setDefaultCloseOperation(3);//关闭模式
-        /*
-            0 DO_NOTHING_ON_CLOSE &&
-            1 HIDE_ON_CLOSE &&
-            2 DISPOSE_ON_CLOSE &&
-            3 EXIT_ON_CLOSE
-        */
-        firstFrame.setTitle("2048");//窗体标题 左上
+        new JavaFrameTest();
+    }
 
-        firstFrame.setLocationRelativeTo(null);//窗体居中
-        firstFrame.setAlwaysOnTop(true);//窗体永远在顶层
+    @Override
+    public void mouseClicked(MouseEvent e) {
 
     }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+//        Random rd = new Random();
+//        btn0.setBounds(rd.nextInt(0,300),rd.nextInt(0,300),100,100);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        System.out.println(keyCode);
+    }
 }
+
+//class ActionListenerImpl implements ActionListener {
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//        System.out.println("DING~");
+//    }
+//}
+//class MouseListenerImpl implements MouseListener{
+//
+//    @Override
+//    public void mouseClicked(MouseEvent e) {
+//        System.out.println("clicked");
+//    }
+//
+//    @Override
+//    public void mousePressed(MouseEvent e) {
+//        System.out.println("pressed");
+//    }
+//
+//    @Override
+//    public void mouseReleased(MouseEvent e) {
+//        System.out.println("released");
+//    }
+//
+//    @Override
+//    public void mouseEntered(MouseEvent e) {
+//        System.out.println("enter");
+//    }
+//
+//    @Override
+//    public void mouseExited(MouseEvent e) {
+//        System.out.println("exit");
+//    }
+//}
